b9b31e46acc5cd062b3be65ae2105b99b231ed5e
A. T. <63013822+WRABZY@users.noreply.github.com>
2026-03-09 02:31:22 +0300

    Added sound on linux

--- flake.nix
+++ flake.nix
@@ -37,6 +37,11 @@
             glew
             freeglut
             patchelf
+
+            alsa-lib
+            alsa-plugins
+            pipewire
+            pulseaudio
           ];
 
           shellHook = ''
@@ -46,6 +51,29 @@
             # Автоматически добавляет пути библиотек в LD_LIBRARY_PATH
             export LD_LIBRARY_PATH=${pkgs.libGL}/lib:${pkgs.mesa}/lib:${pkgs.libX11}/lib:$LD_LIBRARY_PATH
             echo "OpenGL libraries loaded from: ${pkgs.libGL}/lib"
+
+            # Используем ALSA-плагины из того же pipewire
+                        export ALSA_PLUGIN_DIR=${pkgs.pipewire}/lib/alsa-lib
+
+                        # Создаем конфиг для ALSA через PipeWire
+                        cat > ~/.asoundrc << EOF
+                        pcm.!default {
+                            type pipewire
+                            hint.description "Default ALSA device via PipeWire"
+                        }
+                        ctl.!default {
+                            type pipewire
+                            hint.description "Default control via PipeWire"
+                        }
+            EOF
+
+                        # Пробрасываем сокет PipeWire из системы
+                        export XDG_RUNTIME_DIR=/run/user/$(id -u)
+                        export PIPEWIRE_RUNTIME_DIR=$XDG_RUNTIME_DIR
+                        export LD_PRELOAD=/nix/store/97wpr90s0krpq4rv9jdqbr8aavfifgd4-alsa-lib-1.2.15.1/lib/libasound.so.2
+
+                        echo "Sound configured with PipeWire from ${pkgs.pipewire}"
+
             echo "Ebitengine development environment loaded"
           '';
         };
